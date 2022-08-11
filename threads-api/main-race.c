5e9d8866d38773c619d62f35b47bc4327e9d6556
Ahmed Araby <ahmedaraby605@gmail.com>
2022-08-11 23:35:50 +0200

    prevent the data rece using mutex lock

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -3,16 +3,21 @@
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Pthread_mutex_lock(&mlock);
+    balance++; // unprotected access
+    Pthread_mutex_unlock(&mlock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    Pthread_mutex_lock(&mlock);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&mlock);
     Pthread_join(p, NULL);
     return 0;
 }
